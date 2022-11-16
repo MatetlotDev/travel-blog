62a182239c4e94ab7fc26321a8abc3d2a0d51dcb
Matthias Lechien <matthias.lechien@famoco.com>
2022-11-16 21:44:31 +0100

    ci: configure color theme & fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css';
+import { ThemeProvider } from '@emotion/react';
+import { Montserrat } from '@next/font/google';
 import type { AppProps } from 'next/app';
+import '../styles/globals.css';
+import { defaultTheme } from '../utils/theme';
+
+const montserrat = Montserrat();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={montserrat.className}>
+      <ThemeProvider theme={defaultTheme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </main>
+  );
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,28 @@
 import styled from '@emotion/styled';
 import type { NextPage } from 'next';
 
-const Title = styled.div`
-  color: red;
+const Title = styled.h1`
+  background: ${({ theme: { colors } }) => colors.primary.main};
+  color: ${({
+    theme: {
+      colors: { secondary },
+    },
+  }) => secondary[80]};
+  font-size: 47px;
+  font-weight: 200;
 `;
+const Paragraph = styled.p`
+  font-style: italic;
+  font-weight: 100;
+`
 
 const Home: NextPage = () => {
-  return <Title>Hello world !</Title>;
+  return (
+    <>
+      <Title>Hello world !</Title>
+      <Paragraph>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae ut laborum commodi? Similique molestiae consequatur, ut ratione eum atque blanditiis obcaecati dolorem. Ab officiis veritatis, ut numquam praesentium eos sunt!</Paragraph>
+    </>
+  );
 };
 
 export default Home;
