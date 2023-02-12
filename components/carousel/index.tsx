410907d9069a682a21b1ed400c4001d8122494a2
Matthias Lechien <matthias.lechien@famoco.com>
2023-02-12 14:18:25 +0100

    feat: finish carousel component

--- components/carousel/index.tsx
+++ components/carousel/index.tsx
@@ -1,11 +1,149 @@
-import styled from '@emotion/styled';
+import Image from 'next/image';
+import { useMemo, useState } from 'react';
+import { SlArrowLeft, SlArrowRight } from 'react-icons/sl';
+import {
+  BackdropCarousel,
+  CarouselWrapper,
+  ChangeImgButton,
+  ImageDescription,
+} from 'widgets/homepage/wrappers';
 
-const Carousel = () => <CarouselWrapper></CarouselWrapper>;
+interface image {
+  path: string;
+  country: string;
+  region: string;
+  date: Date;
+  [key: string]: any;
+}
+
+const mockedImages: image[] = [
+  {
+    path: '/menu/menu1.jpg',
+    country: 'Italie',
+    region: 'Dolomites',
+    date: new Date('2022-03-25'),
+  },
+  {
+    path: '/menu/menu2.jpg',
+    country: 'Suisse',
+    region: 'Valais',
+    date: new Date('2022-12-05'),
+  },
+  {
+    path: '/menu/menu3.jpg',
+    country: 'Sicile',
+    region: 'Sicile',
+    date: new Date('2022-02-09'),
+  },
+  {
+    path: '/menu/menu4.jpg',
+    country: 'France',
+    region: 'Jura',
+    date: new Date('2021-01-20'),
+  },
+  {
+    path: '/menu/menu5.jpg',
+    country: 'France',
+    region: 'Hautes-savoies',
+    date: new Date('2022-03-25'),
+  },
+  {
+    path: '/menu/menu6.jpg',
+    country: 'Suisse',
+    region: 'Valais',
+    date: new Date('2022-12-05'),
+  },
+  {
+    path: '/menu/menu7.jpg',
+    country: 'Sicile',
+    region: 'Sicile',
+    date: new Date('2022-02-09'),
+  },
+  {
+    path: '/menu/menu8.jpg',
+    country: 'France',
+    region: 'Jura',
+    date: new Date('2021-01-20'),
+  },
+  {
+    path: '/menu/menu9.jpg',
+    country: 'France',
+    region: 'Hautes-savoies',
+    date: new Date('2022-03-25'),
+  },
+];
+
+const Carousel = () => {
+  const [fakeIndex, setFakeIndex] = useState(0);
+  const [actual, setActual] = useState(1);
+
+  const images = useMemo(() => {
+    return mockedImages.map((img, idx) => {
+      const index =
+        idx + fakeIndex >= mockedImages.length
+          ? idx + fakeIndex - mockedImages.length
+          : idx + fakeIndex;
+
+      let className;
+      if (index === 0) className = 'left';
+      else if (index === 1) className = 'center';
+      else if (index === 2) className = 'right';
+      else className = 'behind';
+      return { ...img, className };
+    });
+  }, [fakeIndex]);
+
+  const handleNext = () => {
+    if (fakeIndex === 0) setFakeIndex(mockedImages.length - 1);
+    else setFakeIndex(fakeIndex - 1);
+
+    if (actual === mockedImages.length - 1) setActual(0);
+    else setActual(actual + 1);
+  };
+
+  const handlePrev = () => {
+    if (fakeIndex < mockedImages.length - 1) setFakeIndex(fakeIndex + 1);
+    else setFakeIndex(0);
+
+    if (actual === 0) setActual(mockedImages.length - 1);
+    else setActual(actual - 1);
+  };
+
+  const description = useMemo(() => {
+    const imgDate = mockedImages[actual]?.date.toLocaleDateString('fr-BE', {
+      month: 'long',
+      year: 'numeric',
+    });
+
+    return `${mockedImages[actual]?.region} (${
+      mockedImages[actual]?.country
+    }), ${imgDate.charAt(0).toUpperCase()}${imgDate.slice(1)}`;
+  }, [actual]);
+
+  return (
+    <CarouselWrapper>
+      {images.map((img) => {
+        return (
+          <div key={img.path} className={`image-wrapper ${img.className}`}>
+            <Image
+              src={img.path}
+              width={350}
+              height={510}
+              alt="group of people"
+            />
+          </div>
+        );
+      })}
+      <BackdropCarousel id="backdrop-carousel" />
+      <ChangeImgButton onClick={handlePrev} className="left-arrow">
+        <SlArrowLeft />
+      </ChangeImgButton>
+      <ChangeImgButton onClick={handleNext} className="right-arrow">
+        <SlArrowRight />
+      </ChangeImgButton>
+      <ImageDescription>{description}</ImageDescription>
+    </CarouselWrapper>
+  );
+};
 
 export default Carousel;
-
-const CarouselWrapper = styled.div`
-  width: 50vw;
-  height: 85vmin;
-  background: red;
-`;
--- widgets/homepage/wrappers.ts
+++ widgets/homepage/wrappers.ts
@@ -228,3 +228,98 @@ export const Texts = styled.div`
     width: 90%;
   }
 `;
+export const CarouselWrapper = styled.div`
+  width: 50vw;
+  height: 85vmin;
+  position: relative;
+
+  .image-wrapper {
+    position: absolute;
+    transition: all 0.5s;
+
+    img {
+      object-fit: cover;
+    }
+  }
+
+  .center {
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 10;
+    cursor: pointer;
+
+    &:hover {
+      transform: translate(-50%, -50%) rotate(-1deg) scale(1.08);
+    }
+  }
+  .left {
+    top: 50%;
+    left: 10%;
+    transform: translateY(-50%) scale(0.8) rotate(-2deg);
+    z-index: 8;
+  }
+  .right {
+    top: 50%;
+    right: 10%;
+    transform: translateY(-50%) scale(0.8) rotate(2deg);
+    z-index: 8;
+  }
+  .behind {
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%) scale(0.01);
+    opacity: 0;
+    z-index: 7;
+  }
+`;
+export const BackdropCarousel = styled.div`
+  z-index: 9;
+  width: 500px;
+  height: 300px;
+  background: black;
+  position: absolute;
+  border-radius: 50%;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  filter: blur(150px);
+  transition: opacity 0.25s;
+`;
+export const ChangeImgButton = styled.button`
+  border: 1px dashed ${({ theme }) => theme.colors.white.main};
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  color: ${({ theme }) => theme.colors.white.main};
+  background: none;
+  border-radius: 50%;
+  padding: 20px;
+  font-size: 2.5rem;
+  z-index: 11;
+  width: 67px;
+  height: 67px;
+  cursor: pointer;
+  transition: all 0.4s;
+
+  &:hover {
+    background: ${({ theme }) => theme.colors.white.main};
+    color: ${({ theme }) => theme.colors.black.main};
+    border-color: ${({ theme }) => theme.colors.black.main};
+    opacity: 0.7;
+  }
+
+  &.left-arrow {
+    left: 24%;
+  }
+  &.right-arrow {
+    right: 24%;
+  }
+`;
+export const ImageDescription = styled.p`
+  position: absolute;
+  bottom: 5%;
+  left: 25%;
+  z-index: 10;
+  font-size: 1.8rem;
+`;
