714d1f68c1e76f59f4e0c79f12f7e794cd618c78
Matthias Lechien <matthias.lechien@famoco.com>
2023-09-02 20:00:54 +0200

    feat: create diaryDays constant

--- constants/global.ts
+++ constants/global.ts
@@ -140,3 +140,104 @@ export const mockedArticles: Article[] = [
     date: new Date('02/22/2021'),
   },
 ];
+
+export type DiaryDay = {
+  id: number;
+  date: Date;
+  title: string;
+  text: string;
+  pictures: string[];
+};
+
+export const diaryDays: DiaryDay[] = [
+  {
+    id: 1,
+    date: new Date('12/08/2023'),
+    title: 'Aménagement du van !',
+    text: 'On sait depuis longtemps que travailler avec du texte lisible et contenant du sens est source de distractions, et empêche de se concentrer sur la mise en page elle-même. L"avantage du Lorem Ipsum sur un texte générique comme "Du texte. Du texte. Du texte." est qu"il possède une distribution de lettres plus ou moins normale, et en tout cas comparable avec celle du français standard. De nombreuses suites logicielles de mise en page ou éditeurs de sites Web ont fait du Lorem Ipsum leur faux texte par défaut, et une recherche pour "Lorem Ipsum" vous conduira vers de nombreux sites qui n"en sont encore qu"à leur phase de construction. Plusieurs versions sont apparues avec le temps, parfois par accident, souvent intentionnellement.',
+    pictures: [
+      '/menu/menu1.jpg',
+      '/menu/menu2.jpg',
+      '/menu/menu3.jpg',
+      '/menu/menu4.jpg',
+      '/menu/menu5.jpg',
+      '/menu/menu6.jpg',
+      '/menu/menu7.jpg',
+      '/menu/menu8.jpg',
+      '/menu/menu9.jpg',
+      '/menu/menu10.jpg',
+      '/menu/menu11.jpg',
+    ],
+  },
+  {
+    id: 2,
+    date: new Date('20/08/2023'),
+    title: 'Grande voie dans le verdon.',
+    text: 'On sait depage elle-même. L"avantage du Lorem Ipsum sur un texte générique comme "Du texte. Du texte. Du texte." est qu"il possède une distribution de lettres plus ou moins normale, et en tout cas comparable avec celle du français standard. De nombreuses suites logicielles de mise en page ou éditeurs de sites Web ont fait du Lorem Ipsum leur faux texte par défaut, et une recherche pour "Lorem Ipsum" vous conduira vers de nombreux sites qui n"en sont encore qu"à leur phase de construction. Plusieurs versions sont apparues avec le temps, parfois par accident, souvent intentionnellement.',
+    pictures: [
+      '/menu/menu1.jpg',
+      '/menu/menu2.jpg',
+      '/menu/menu3.jpg',
+      '/menu/menu4.jpg',
+      '/menu/menu5.jpg',
+      '/menu/menu6.jpg',
+      '/menu/menu7.jpg',
+      '/menu/menu8.jpg',
+      '/menu/menu9.jpg',
+    ],
+  },
+  {
+    id: 3,
+    date: new Date('30/08/2023'),
+    title: 'Grimpe dans le Pfalz',
+    text: 'Du sens est source de distractions, et empêche de se concentrer sur la mise en page elle-même. L"avantage du Lorem Ipsum sur un texte générique comme "Du texte. Du texte. Du texte." est qu"il possède une distribution de lettres plus ou moins normale, et en tout cas comparable avec celle du français standard. De nombreuses suites logicielles de mise en page ou éditeurs de sites Web ont fait du Lorem Ipsum leur faux texte par défaut, et une recherche pour "Lorem Ipsum" vous conduira vers de nombreux sites qui n"en sont encore qu"à leur phase de construction. Plusieurs versions sont apparues avec le temps, parfois par accident, souvent intentionnellement.',
+    pictures: [
+      '/menu/menu1.jpg',
+      '/menu/menu2.jpg',
+      '/menu/menu3.jpg',
+      '/menu/menu4.jpg',
+      '/menu/menu5.jpg',
+      '/menu/menu6.jpg',
+    ],
+  },
+  {
+    id: 4,
+    date: new Date('01/09/2023'),
+    title: 'Visite de Bolzano',
+    text: 'On sait depuis. De nombreuses suites logicielles de mise en page ou éditeurs de sites Web ont fait du Lorem Ipsum leur faux texte par défaut, et une recherche pour "Lorem Ipsum" vous conduira vers de nombreux sites qui n"en sont encore qu"à leur phase de construction. Plusieurs versions sont apparues avec le temps, parfois par accident, souvent intentionnellement.',
+    pictures: [
+      '/menu/menu1.jpg',
+      '/menu/menu2.jpg',
+      '/menu/menu3.jpg',
+      '/menu/menu4.jpg',
+      '/menu/menu5.jpg',
+      '/menu/menu6.jpg',
+      '/menu/menu9.jpg',
+      '/menu/menu10.jpg',
+      '/menu/menu11.jpg',
+    ],
+  },
+  {
+    id: 5,
+    date: new Date('03/09/2023'),
+    title: 'Grande voie sur les tours de sella',
+    text: 'À leur phase de construction. Plusieurs versions sont apparues avec le temps, parfois par accident, souvent intentionnellement.',
+    pictures: [
+      '/menu/menu1.jpg',
+      '/menu/menu5.jpg',
+      '/menu/menu6.jpg',
+      '/menu/menu7.jpg',
+      '/menu/menu8.jpg',
+      '/menu/menu9.jpg',
+      '/menu/menu10.jpg',
+      '/menu/menu11.jpg',
+    ],
+  },
+  {
+    id: 6,
+    date: new Date('11/09/2023'),
+    title: 'Aménagement du van !',
+    text: 'On sait depuis longtemps que travailler avec du texte lisible et contenant du sens est source de distractions, et empêche de se concentrer sur la mise en page elle-même. L"avantage du Lorem Ipsum sur un texte générique comme "Du texte. Du texte. Du texte." est qu"il possède une distribution de lettres plus ou moins normale, et en tout cas comparable avec celle du français standard. De nombreuses suites logicielles de mise en page ou éditeurs de sites Web ont fait du Lorem Ipsum leur faux texte par défaut, et une recherche pour "Lorem Ipsum" vous conduira vers de nombreux sites qui n"en sont encore qu"à leur phase de construction. Plusieurs versions sont apparues avec le temps, parfois par accident, souvent intentionnellement.',
+    pictures: ['/menu/menu1.jpg', '/menu/menu2.jpg', '/menu/menu3.jpg'],
+  },
+];
--- pages/journal.tsx
+++ pages/journal.tsx
@@ -1,4 +1,6 @@
+import styled from '@emotion/styled';
 import { HistoryNavigation } from 'components';
+import { diaryDays } from 'constants/global';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
@@ -8,12 +10,20 @@ const Diary: NextPage = () => {
       <Head>
         <title>Journal</title>
       </Head>
-      <HistoryNavigation
-        firstDate={new Date('22/05/2022')}
-        lastDate={new Date('12/08/2023')}
-      />
+      <ContentWrapper>
+        <HistoryNavigation
+          firstDate={new Date('22/05/2022')}
+          lastDate={new Date('12/08/2023')}
+        />
+        {/* {diaryDays.map(day => )} */}
+      </ContentWrapper>
     </>
   );
 };
 
 export default Diary;
+
+const ContentWrapper = styled.div`
+  margin-left: 130px;
+  min-height: 100vh;
+`;
