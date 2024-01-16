1f787bd7b58466c3b679ccd719efa89c3f51f2e6
Matthias Lechien <matthias.lechien@famoco.com>
2024-01-16 22:29:59 +0200

    feat: handle form submission part 1

--- app/admin/diary/page.tsx
+++ app/admin/diary/page.tsx
@@ -1,3 +1,33 @@
+import { handleSubmitDiaryForm } from '@/app/actions';
+import styles from './style.module.scss';
+
 export default function DiaryAdminPage() {
-  return <h1>Diary Admin Page</h1>;
+  return (
+    <div className={styles.wrapper}>
+      <form action={handleSubmitDiaryForm}>
+        <div className={styles.formGroup}>
+          <label htmlFor="title">Titre</label>
+          <input name="title" type="text" id="title" />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="date">Date</label>
+          <input
+            defaultValue={new Date().toISOString().split('T')[0]}
+            type="date"
+            name="date"
+            id="date"
+          />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="text">Texte</label>
+          <textarea name="text" id="text"></textarea>
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="pictures">Images</label>
+          <input type="file" name="pictures" id="pictures" multiple />
+        </div>
+        <button type="submit">Sauver</button>
+      </form>
+    </div>
+  );
 }
