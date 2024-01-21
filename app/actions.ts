1cd5632dba44c87b0ae4e2d9f8ad81822b439855
Matthias Lechien <matthias.lechien@famoco.com>
2024-01-21 16:40:04 +0200

    feat: finish uploading dairy feature

--- app/actions.ts
+++ app/actions.ts
@@ -1,56 +1,7 @@
 'use server';
 
 import { RedirectType, redirect } from 'next/navigation';
-import { v4 as uuid } from 'uuid';
 
-export async function navigate(path: string, type: RedirectType) {
+export async function navigate(path: string, type?: RedirectType) {
   redirect(path, type);
-}
-
-// async function uploadImage(docId, images) {
-//   const filePath = `images/${docId}/${image.name}`;
-//   const newImageRef = ref(storage, filePath);
-//   await uploadBytesResumable(newImageRef, image);
-
-//   return await getDownloadURL(newImageRef);
-// }
-
-export async function handleSubmitDiaryForm(formData: FormData) {
-  /**
-   * retrieve images
-   * add them to firestore storage
-   * take back urls
-   * create an object with all the data
-   * add it to firestore
-   *  */
-
-  const docId = uuid();
-
-  // const imageUrl = await uploadImages(formData.get('pictures') as File)
-
-  // console.log('URL : ', imageUrl);
-
-  // formData.getAll('pictures').forEach(async (image) => {
-  //   const imageUrl = await uploadImage(image as File);
-  //   console.log('URL : ', imageUrl);
-  // });
-
-  const docContent = {
-    id: docId,
-    title: formData.get('title'),
-    text: formData.get('text'),
-    date: formData.get('date'),
-    pictures: [],
-  };
-
-  // try {
-  //   // create a document and add it to firestore collection 'diary'
-  //   const diaryRef = doc(db, 'diary', docContent.id);
-  //   await setDoc(diaryRef, docContent);
-  // } catch (error) {
-  //   console.error('Error while saving Diary form : ', error);
-  //   throw error;
-  // }
-
-  // redirect('/admin');
 }
--- app/admin/diary/page.tsx
+++ app/admin/diary/page.tsx
@@ -1,50 +1,105 @@
 'use client';
 
+import { navigate } from '@/app/actions';
+import { db } from '@/firebase/config';
 import { uploadImage } from '@/firebase/storage';
+import { doc, setDoc } from 'firebase/firestore';
+import { useState } from 'react';
+import { v4 as uuid } from 'uuid';
 import styles from './style.module.scss';
 
-const handleSubmitDiaryForm = async (e: React.FormEvent<HTMLFormElement>) => {
-  e.preventDefault();
-  const form = e.currentTarget;
-  const formData = new FormData(form);
-
-  // urls of the uploaded images
-  const pictureUrls = await Promise.all(
-    formData.getAll('pictures').map((file) => uploadImage(file))
-  );
-
-  // create a doc in images collection for each image
-
-  // create a doc in diary collection with the images Ids
-};
-
 export default function DiaryAdminPage() {
+  const [uploading, setUploading] = useState(false);
+  const [uploadingStatus, setUploadingStatus] = useState<null | string>(null);
+  const [error, setError] = useState<null | string>(null);
+
+  const handleSubmitDiaryForm = async (e: React.FormEvent<HTMLFormElement>) => {
+    setUploading(true);
+    e.preventDefault();
+    const form = e.currentTarget;
+    const formData = new FormData(form);
+
+    try {
+      setUploadingStatus('Starting to uploading images.');
+
+      // urls of the uploaded images
+      const pictureUrls = await Promise.all(
+        formData.getAll('pictures').map((file) => uploadImage(file))
+      );
+
+      setUploadingStatus(
+        'Uploading images finished. Creating images documents.'
+      );
+
+      // create a doc in images collection for each image and returns the Ids of the documents
+      const imageIds = await Promise.all(
+        pictureUrls.map(async (url) => {
+          const imageId = uuid();
+          const imageRef = doc(db, 'images', imageId);
+          await setDoc(imageRef, { url });
+          return imageId;
+        })
+      );
+      const docId = uuid();
+
+      // create a doc in diary collection with the images Ids
+      const docContent = {
+        id: docId,
+        title: formData.get('title'),
+        text: formData.get('text'),
+        date: formData.get('date'),
+        pictures: imageIds,
+      };
+
+      setUploadingStatus('Images documents uploaded. Creating diary document.');
+
+      const diaryRef = doc(db, 'diary', docContent.id);
+      await setDoc(diaryRef, docContent);
+      setUploading(false);
+      navigate('/admin');
+    } catch (err) {
+      setUploading(false);
+      console.error(err);
+      setError('Error has occured while uploading images');
+    }
+  };
+
   return (
     <div className={styles.wrapper}>
-      <form onSubmit={handleSubmitDiaryForm}>
-        <div className={styles.formGroup}>
-          <label htmlFor="title">Titre</label>
-          <input name="title" type="text" id="title" />
-        </div>
-        <div className={styles.formGroup}>
-          <label htmlFor="date">Date</label>
-          <input
-            defaultValue={new Date().toISOString().split('T')[0]}
-            type="date"
-            name="date"
-            id="date"
-          />
-        </div>
-        <div className={styles.formGroup}>
-          <label htmlFor="text">Texte</label>
-          <textarea name="text" id="text"></textarea>
-        </div>
-        <div className={styles.formGroup}>
-          <label htmlFor="pictures">Images</label>
-          <input type="file" name="pictures" id="pictures" multiple />
-        </div>
-        <button type="submit">Sauver</button>
-      </form>
+      {uploading ? (
+        <>
+          <div className={styles.loader} />
+          <p>{uploadingStatus}</p>
+        </>
+      ) : (
+        <>
+          <form onSubmit={handleSubmitDiaryForm}>
+            <div className={styles.formGroup}>
+              <label htmlFor="title">Titre</label>
+              <input name="title" type="text" id="title" />
+            </div>
+            <div className={styles.formGroup}>
+              <label htmlFor="date">Date</label>
+              <input
+                defaultValue={new Date().toISOString().split('T')[0]}
+                type="date"
+                name="date"
+                id="date"
+              />
+            </div>
+            <div className={styles.formGroup}>
+              <label htmlFor="text">Texte</label>
+              <textarea name="text" id="text"></textarea>
+            </div>
+            <div className={styles.formGroup}>
+              <label htmlFor="pictures">Images</label>
+              <input type="file" name="pictures" id="pictures" multiple />
+            </div>
+            <button type="submit">Sauver</button>
+          </form>
+          <p>{error}</p>
+        </>
+      )}
     </div>
   );
 }
